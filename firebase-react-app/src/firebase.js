fc99d03af2bf363695efcaf696ca8199eb583008
Noah <noahmichaels@gmail.com>
2023-11-08 21:08:02 -0500

    firestore creation
    Import the firestore database and create the file for it

---
+++ firebase-react-app/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDdQr0vNIMRYvHQXfxbRlLrpuUAmRk5DOM",
+  authDomain: "todoapp-3a492.firebaseapp.com",
+  projectId: "todoapp-3a492",
+  storageBucket: "todoapp-3a492.appspot.com",
+  messagingSenderId: "366888343318",
+  appId: "1:366888343318:web:8d30459f70db22a1966237"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
